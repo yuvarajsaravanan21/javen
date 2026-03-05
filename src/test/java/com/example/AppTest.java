7f796a86853c14231a634ab977a616a871ec2c27
yuvaraj S <yuvaraj.saravanan2023@vitstudent.ac.in>
2026-03-05 07:28:07 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,7 +2,7 @@ package com.exampe;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class AppTest{
-    @test
+    @Test
     public void testadd(){
         App app=new App();
         assertEquals(5,app.add(2,3));
